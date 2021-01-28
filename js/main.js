93a893de7703f89cf8ca4a0f71185b3d7dedfc35
AhmedKoga <valentino-koga@hotmail.com>
2021-01-28 15:27:40 +0100

    push cv

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// function myFunction() {
+//     var p = ["JE SUIS Ahmed GHANEM LAKHAL DÉVELOPPEUR WEB|"];
+//     for (var i = 0; i <= 43; i++) {
+//         console.log(i)
+//         p.forEach(element => console.log(element[i]));
+//     }
+//     setInterval(function() {
+//         document.getElementById('pMove').innerHTML = pop.element[i];
+//     }, 1000);
+// }
+// function myFunction() {
+//     var p = "JE SUIS Ahmed GHANEM LAKHAL DÉVELOPPEUR WEB<span id='slash'>|</span>";
+//     setInterval(function() {
+//         document.getElementById('pMove').innerHTML = p;
+//     }, 2000);
+//     setInterval(function() {
+//         document.getElementById('pMove').innerHTML = "";
+//     }, 6000);
+// }
